8cb807d5ea2fadeb3eddccb495952b8e18252451
Jose Bustillo <179273869+Fern-labs@users.noreply.github.com>
2025-03-25 18:13:26 -0700

    Finished project styled css and added starter canvas and grid change button.

--- etchASketch.js
+++ etchASketch.js
@@ -1,31 +1,64 @@
-function createDiv(userInput) {
+let count = 0;
+function createDiv(squareNum) {
   const div = document.createElement("div");
   const cont = document.querySelector(".container");
   cont.appendChild(div);
 
-  let containerSize = userInput * 20;
-
-  cont.style.height = containerSize + "px";
-  cont.style.flexBasis = containerSize + "px";
+  div.style.height = squareNum + "px";
+  div.style.flexBasis = squareNum + "px";
 }
 
-function createDivRow(num) {
-  for (i = 0; i < num; i++) {
-    let userNum = Math.sqrt(num);
-    createDiv(userNum);
+function createDivRow(squareNum) {
+  let squaredSize = 960 / squareNum;
+  let builtSquared = squaredSize * squaredSize;
+  for (i = 0; i < builtSquared; i++) {
+    createDiv(squareNum);
   }
 }
+function deleteDivs() {
+  document.querySelectorAll(".container>div").forEach((elem) => {
+    elem.remove();
+  });
+}
 
-let userInput = prompt(
-  "How big do you want your Canvas hint 16*16 Enter only one num"
-);
-let numTwice = parseInt(userInput * userInput);
-createDivRow(numTwice);
+function divSizeAndHover() {
+  deleteDivs();
 
-document.querySelectorAll("div").forEach((elem) => {
-  if (!elem.classList.contains("container")) {
-    elem.addEventListener("mouseover", () => {
-      elem.style.backgroundColor = "black";
-    });
+  count++;
+  let n = prompt(
+    "Please enter the size for your canvas (e.g., 16 for a 16x16 canvas). Maximum size is 100:"
+  );
+  if (n > 100) {
+    alert("Keep it to Less than 100");
+  } else {
+    let squareNum = 960 / n;
+    createDivRow(squareNum);
   }
-});
+
+  document.querySelectorAll("div").forEach((elem) => {
+    if (!elem.classList.contains("container")) {
+      elem.addEventListener("mouseover", () => {
+        let newRandomColor = Math.floor(Math.random() * 16777215).toString(16);
+        elem.style.backgroundColor = "#" + newRandomColor;
+      });
+    }
+  });
+}
+
+function starterCanvas() {
+  createDivRow(64);
+
+  document.querySelectorAll("div").forEach((elem) => {
+    if (!elem.classList.contains("container")) {
+      elem.addEventListener("mouseover", () => {
+        let newRandomColor = Math.floor(Math.random() * 16777215).toString(16);
+        elem.style.backgroundColor = "#" + newRandomColor;
+      });
+    }
+  });
+}
+
+const buttonSize = document.querySelector(".size");
+buttonSize.addEventListener("click", divSizeAndHover);
+
+starterCanvas();
